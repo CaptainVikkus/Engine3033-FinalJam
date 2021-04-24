919f03f83c5dcea365d6c4c03c36a2a6d1321dce
Jake <jaketreleaven@gmail.com>
2021-04-23 22:04:56 -0400

    Better Fruit Scene and Objective

--- Assets/Scripts/UI/ObjectiveTextBehaviour.cs
+++ Assets/Scripts/UI/ObjectiveTextBehaviour.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 public class ObjectiveTextBehaviour : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI objective;
-    [SerializeField] float displaySeconds = 20f;
+    [SerializeField] float displaySeconds = 2f;
 
     private float timer;
+    private Color start;
 
     // Start is called before the first frame update
     void Start()
     {
         timer = 0;
+        start = objective.color;
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class ObjectiveTextBehaviour : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        objective.color = Color.Lerp(objective.color, Color.clear, timer / displaySeconds);
+        objective.color = Color.Lerp(start, Color.clear, timer / displaySeconds);
 
         if (timer >= displaySeconds)
             Destroy(gameObject);
